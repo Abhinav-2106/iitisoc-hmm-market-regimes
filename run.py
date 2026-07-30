acd371765f9fff521ead148a51b7650deb8d2de2
Shubham Sharma <shubhamsharma024680@gmail.com>
2026-07-30 18:51:59 +0530

    some minor changes

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from subprocess import run
 import sys
 
 steps= [
-    # ("Train HMM and feature engineering", ["python", "src/hmm/base_hmm_model.py"]),
+    ("Train HMM and feature engineering", ["python", "src/hmm/base_hmm.py"]),
     ("Generating forcast datasets", ["python", "src/strategy/transition_forecast.py"]),
     ("Running Strategy 1", ["python", "src/strategy/strategy_engine_1.py"]),
     ("Running Strategy 2", ["python", "src/strategy/strategy_engine_2.py"]),
